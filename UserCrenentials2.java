15227d470724d13b0e9a6ef76a3fa423586fb85c
Anthati Aravind kumar <anthatiaravindkumar0@gmail.com>
2025-01-03 10:50:25 +0530

    User Credentials using while loop

---
+++ UserCrenentials2.java
@@ -0,0 +1,9 @@
+public class UserCredentials2{
+    public static void main(String[] args){
+        String username =  "Aravind";
+        String password =  "Aravind@123";
+        while((username == "Aravind") && (password == "Aravind@123")){
+            System.out.println("Hello Aravind welcome to Mallareddy University");
+            break;
+        }
+}
